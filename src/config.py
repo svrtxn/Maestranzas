8acf2d7452e116fbd1a5e7c5afd9dc1b30ddd6cc
FcaHJ <fran.herrera@duocuc.cl>
2024-06-15 19:00:43 -0400

    Se creo config.py

---
+++ src/config.py
@@ -0,0 +1,15 @@
+class Config:
+    SECRET_KEY = 'B!1weNAt1T^%kvhUI*S^'
+
+#Nombre y contraseña de la base de datos
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'ej_maestranzas'
+    MYSQL_CURSORCLASS = 'DictCursor'
+
+config = {
+    'development': DevelopmentConfig
+}
